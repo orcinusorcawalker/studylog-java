25a6f1f800e8b6d57d499787c4f0b6dfb1933c6f
orcinusorcawalker <42757052+orcinusorcawalker@users.noreply.github.com>
2018-09-07 00:49:08 +1200

    Update secondassss2.java

--- ass-one/secondassss2.java
+++ ass-one/secondassss2.java
@@ -5,7 +5,7 @@ public class secondassss2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+                arithmeticTable();
 	}
 	static void arithmeticTable()
 	{
