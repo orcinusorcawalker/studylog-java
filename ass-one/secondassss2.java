80a2e7335e6a905d1e42725ea2e73551968db8ce
orcinusorcawalker <42757052+orcinusorcawalker@users.noreply.github.com>
2018-09-06 17:31:09 +1200

    Add files via upload

---
+++ ass-one/secondassss2.java
@@ -0,0 +1,29 @@
+package assssssssssssssssssss;
+import static java.lang.System.out;
+import java.util.Scanner;
+public class secondassss2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	static void arithmeticTable()
+	{
+		double height,width;
+		try(Scanner input = new Scanner(System.in))
+		{
+			out.print("Enter a height: ");
+			height = input.nextDouble();
+			out.print("Enter a width: ");
+			width = input.nextDouble();
+		}
+		double perimeter = (height + width) * 2;
+		double area = height * width ;
+		String a1 = "Height", a2 = "Width";
+		String a3 = "Area", a4 = "Perimeter";
+		String fmt = "%-20s%-20s%-20s%-20s\n";
+
+
+	}
+
+}
